a8ebbbca180653c1312d5c5e8971496f17bb5a5b
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-04-25 22:20:30 +0200

    pylint/checkers/__init__.py import message sink

--- pylint/checkers/__init__.py
+++ pylint/checkers/__init__.py
@@ -45,6 +45,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Literal
 
 from pylint.checkers.base_checker import (
+    PyLinterMessageSink,
     BaseChecker,
     BaseRawFileChecker,
     BaseTokenChecker,
