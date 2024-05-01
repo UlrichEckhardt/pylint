3fc294261faaf57d04979faa7887003722b664aa
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-05-01 18:12:32 +0200

    extend some for-else tests

--- doc/data/messages/u/undefined-loop-variable/good.py
+++ doc/data/messages/u/undefined-loop-variable/good.py
@@ -1,5 +1,14 @@
-def find_even_number(numbers):
+def find_even_number_1(numbers):
     for x in numbers:
         if x % 2:
             return x
     return None
+
+def find_even_number_2(numbers):
+    for x in numbers:
+        if x % 2:
+            break
+    else:
+        return None
+
+    return x
